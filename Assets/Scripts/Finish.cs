5beb55aca298853353e811c09cf757ff18bbe539
NighterM <melliti100@gmail.com>
2024-04-28 20:51:45 +0100

    fdzdfzdf
    dsdzedfdsfz

--- Assets/Scripts/Finish.cs
+++ Assets/Scripts/Finish.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -8,6 +8,7 @@ public class Finish : MonoBehaviour
     private AudioSource finishSound;
 
     private bool levelCompleted = false;
+
 
     private void Start()
     {
@@ -29,3 +30,4 @@ public class Finish : MonoBehaviour
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 }
+
